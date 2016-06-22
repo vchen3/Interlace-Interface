729870e0533e8b8b2c50c40ab537264ab4a1a7d5
Vivien <vchen3@wellesley.edu>
2016-06-22 14:42:50 -0400

    Reading from complete JSON file! ideaBoxes floating left

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,7 @@ var nameSpace = angular.module("myApp", []);
 nameSpace.controller("GuitarFunction", ['$scope','$http', function($scope,$http)
 		{    
 			$http.get('js/data.json').success(function(data){
-			$scope.guitarVariable = data;
+			$scope.pageData = data;
 		}); 
 
 		}]
