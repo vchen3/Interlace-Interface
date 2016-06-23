434fe65c6f55355d6b0201a126d1f5b144d32836
Vivien <vchen3@wellesley.edu>
2016-06-23 10:05:23 -0400

    Thursday morning code shown to Ethan

--- js/controllers.js
+++ js/controllers.js
@@ -5,6 +5,8 @@ nameSpace.controller("GuitarFunction", ['$scope','$http', function($scope,$http)
 			$http.get('js/data.json').success(function(data){
 			$scope.pageData = data;
 		}); 
-
+		$scope.plusOne = function(index) { 
+  		$scope.pageData.ideas[index].likes += 1; 
+		};
 		}]
 );
