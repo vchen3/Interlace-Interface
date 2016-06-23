d7719f37957811ceb97de1390c10e87746a7a90e
Vivien <vchen3@wellesley.edu>
2016-06-23 13:38:30 -0400

    Ideas load alphabetically by author

--- js/controllers.js
+++ js/controllers.js
@@ -1,23 +1,6 @@
 var nameSpace = angular.module("myApp", []);
 
-
-nameSpace.filter('orderObjectBy', function(){
- return function(input, attribute) {
-    if (!angular.isObject(input)) return input;
-
-    var array = [];
-    for(var objectKey in input) {
-        array.push(input[objectKey]);
-    }
-
-    array.sort(function(a, b){
-        a = parseInt(a.attribute);
-        b = parseInt(b.attribute);
-        return a - b;
-    });
-    return array;
- }
-});
+console.log("starting up");
 
 nameSpace.controller("InterfaceController", ['$scope','$http', function($scope,$http)
 		{    
