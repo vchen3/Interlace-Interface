22b7928025c31e29b0302f8a41779823a7165250
Vivien <vchen3@wellesley.edu>
2016-06-23 17:00:55 -0400

    Connecting 'like' function to idea

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,28 @@
-var nameSpace = angular.module("myApp", []);
+var app = angular.module("myApp", []);
 
 console.log("starting up");
 
-nameSpace.controller("InterfaceController", ['$scope','$http', function($scope,$http)
+app.controller("InterfaceController", ['$scope','$http', function($scope,$http)
 		{    
 			$http.get('js/data.json').success(function(data){
 			$scope.pageData = data;
 		}); 
-		$scope.plusOne = function(index) { 
-  		$scope.pageData.ideas[index].likes += 1; 
+		$scope.plusOne = function(ideaName) { 
+			var ideasArray = $scope.pageData.ideas;
+			for (var i = 0; i<ideasArray.length; i++)
+				console.log(ideasArray[i]);
+			{
+				var trimmedName = ideasArray[i].name.trim();
+				//console.log(trimmedName);
+				var sameName =((trimmedName).localeCompare(ideaName))
+				//console.log(sameName);
+				if (sameName == 0){
+					//console.log(ideasArray[i]);
+					ideasArray[i].likes += 1; 
+					console.log(ideasArray[i].likes);
+				}
+			}
 		};
-		}]
+	}]
 );
+
