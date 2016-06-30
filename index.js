174a35587dc7a71b50a16253ace440788fcdc3d0
Vivien <vchen3@wellesley.edu>
2016-06-30 15:56:17 -0400

    websockets connect and talk

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+//var io = require('socket.io');
+
+app.get('/', function(req, res){
+	console.log("Starting node app");
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(path.join(__dirname, '/public'))); //Add CSS
+app.use('/js', express.static(path.join(__dirname,'/js'))); //Add controller, data
+app.use('/lib', express.static(path.join(__dirname,'/lib'))); //Add Angular and socket?
+
+io.on('connection', function(socket){
+	//console.log('io connected on A');
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+		console.log(msg);
+	});
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+/*var express = require('express');
 var http = require('http');
 var app = express();
 //var http = require('http').Server(app);
@@ -30,4 +59,4 @@ io.on('connection', function(socket){
 
 app.listen(3000, function(){
   console.log('listening on *:3000');
-});
+});*/
