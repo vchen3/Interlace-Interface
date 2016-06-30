4657927dff3747b707ce97eaa2874373bcd1868e
Vivien <vchen3@wellesley.edu>
2016-06-30 11:42:16 -0400

    Running node app with CSS, no angular

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var path = require('path');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	//res.send('Hello World!');
+	console.log('Starting node app');
+  	res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public/img')));
+//app.use(express.static(path.join(__dirname, 'lib')));
+//app.use(express.static(path.join(__dirname, 'js')));
+
 
 //app.use('/js', express.static(path.join(__dirname,'js')));
 //app.use(express.static('/js'));
 //app.use(express.static(__dirname + '/js'));
 //app.use('/static',express.static('js'));
-app.use('/js');
+//app.use('/js');
 //app.get(express.static('js');
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+//io.on('connection', function(socket){
+//  socket.on('chat message', function(msg){
+//    io.emit('chat message', msg);
+//  });
+//});
 
-http.listen(3000, function(){
+app.listen(3000, function(){
   console.log('listening on *:3000');
 });
