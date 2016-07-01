ae3abe61645f573b56a47d7b561960aec7cab0f8
Vivien <vchen3@wellesley.edu>
2016-07-01 09:33:57 -0400

    Clickable buttons update with likes

--- index.js
+++ index.js
@@ -25,38 +25,3 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
-
-/*var express = require('express');
-var http = require('http');
-var app = express();
-//var http = require('http').Server(app);
-//var io = require('socket.io').listen(http);
-var path = require('path');
-
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
-
-app.get('/', function(req, res){
-	console.log("Starting node app");
-  	res.sendFile(__dirname + '/index.html');
-});
-
-app.use(express.static(path.join(__dirname, '/public'))); //Add CSS
-app.use('/js', express.static(path.join(__dirname,'/js'))); //Add controller, data
-app.use('/lib', express.static(path.join(__dirname,'/lib'))); //Add Angular and socket?
-//app.use('/node_modules', express.static(path.join(__dirname,'/node_modules'))); //Add socket?
-
-io.on('connection', function(socket){
-	console.log("io connected");
-});
-//	console.log("io server connected");
-//	socket.on('chat message', function(msg){
-//		io.emit('chat message', msg);
-//   		console.log(ms)g;
-//  });
-//});
-
-app.listen(3000, function(){
-  console.log('listening on *:3000');
-});*/
---
+++ public/controllers.js
@@ -0,0 +1,28 @@
+var app = angular.module("myApp", []);
+
+console.log("starting angular app!!");
+
+app.controller("InterfaceController", ['$scope','$http', function($scope,$http)
+		{    
+			$http.get('js/data.json').success(function(data){
+			$scope.pageData = data;
+		}); 
+		$scope.plusOne = function(ideaName) { 
+			var ideasArray = $scope.pageData.ideas;
+			for (var i = 0; i<ideasArray.length; i++)
+				//console.log(ideasArray[i]);
+			{
+				var idea = ideasArray[i];
+				var trimmedName = idea.name.trim();
+				//console.log(trimmedName);
+				var sameName =((trimmedName).localeCompare(ideaName))
+				if (sameName == 0){
+					console.log(trimmedName);
+					ideasArray[i].likes += 1; 
+					console.log(ideasArray[i].likes);
+				}
+			}
+		};
+	}]
+);
+
