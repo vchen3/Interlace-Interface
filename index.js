a33e499590039b08d6a59156ac658124ff4f559a
Vivien <vchen3@wellesley.edu>
2016-07-06 12:08:59 -0400

    Implementing mongoDB

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
-//var io = require('socket.io');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -14,17 +13,38 @@ app.use(express.static(path.join(__dirname, '/public'))); //Add CSS
 app.use('/js', express.static(path.join(__dirname,'/js'))); //Add controller, data
 app.use('/lib', express.static(path.join(__dirname,'/lib'))); //Add Angular and socket?
 
-//Changed Jul 5
-//io.sockets.on('connection', socket);
+//Conect with socket.io
 io.on('connection', function(socket){
 	socket.on('chatmessage', function(msg){
 		io.emit('chatmessage', msg);
 	});
-	socket.on('like', function(sentData){
-		io.emit('like', sentData);
+	socket.on('like', function(ideaName){
+		io.emit('like', ideaName);
 	});
 });
 
+//Connect with mongoDB
+var MongoClient = require('mongodb').MongoClient;
+var assert = require ('assert');
+var url = 'mongodb://localhost:27017/test';
+
+
+var insertDocument = function(db, callback) {
+	$.getJSON("/public/data.json", function(data){
+   	console.log(data);
+   })
+};
+
+//Connect running mongoDB instance running on localhost port 27017 to test database
+MongoClient.connect(url, function(err,db){
+	assert.equal(null, err);
+	console.log("Connected correctly to server.");
+	insertDocument(db, function(){
+		console.log("inserting data:");
+		db.close();
+	});
+	//db.close();
+}); 
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
