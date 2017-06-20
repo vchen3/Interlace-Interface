7fdcfc13248e6990517511d7d12ea9dd9bb99952
Vivien Chen <vchen@adobe.com>
2017-06-19 17:36:18 -0700

    Removing old study

--- old_study1/routes/background.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET background page. */
-router.get('/background', function(req, res, next) {
-  res.render('background', { title: 'Background' });
-});
-
-module.exports = router;
